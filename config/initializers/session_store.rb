f419253d51550bf897817dc1df031c6cfb0dc0be
Gareth Marshall <garethm@acm.org>
2010-04-15 19:12:23 +1000

    Initial add

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_todo_session',
+  :secret      => '78833edee095eb9c23f13034f3b84ff724074821dceb1e04b0ba2e02c3e3509ddd1459f13cba79935eab78bc297adef55fe0053334e5de899213816edf53245c'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
---
+++ db/migrate/20100415091134_create_todo_items.rb
@@ -0,0 +1,14 @@
+class CreateTodoItems < ActiveRecord::Migration
+  def self.up
+    create_table :todo_items do |t|
+      t.string :title
+      t.boolean :done
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :todo_items
+  end
+end
