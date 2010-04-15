bce197d3161f6d39b4de747c6a2f309195876c8f
Gareth Marshall <garethm@acm.org>
2010-04-15 20:55:37 +1000

    Support done/undo

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,4 +1,6 @@
 class TodoItemsController < ApplicationController
+	before_filter :find_item, :only => [:done, :undo]
+	
 	def index
 		@todoItem = TodoItem.new
 		@todoItems = TodoItem.all( :order => "created_at desc" )
@@ -7,7 +9,7 @@ class TodoItemsController < ApplicationController
 	def create
 		@todoItem = TodoItem.new( params[ :todo_item ] )
 		@todoItem.done = false
-		if @todoItem.save
+		if @todoItem.save!
 			redirect_to root_path
 		else
 			@todoItems = TodoItem.all( :order => "created_at desc" )
@@ -16,7 +18,28 @@ class TodoItemsController < ApplicationController
 	end
 	
 	def destroy
-		TodoItem.delete( params[ :id ] )
+		TodoItem.destroy params[ :id ] 
 		redirect_to root_path
 	end
+	
+	def done
+		@todo_item.done = true
+		save_item
+	end
+	
+	def undo
+		@todo_item.done = false
+		save_item
+	end
+	
+	private
+		def find_item
+			@todo_item = TodoItem.find( params[ :id ] )
+		end
+		
+	private
+		def save_item
+			@todo_item.save!
+			redirect_to root_path
+		end
 end
