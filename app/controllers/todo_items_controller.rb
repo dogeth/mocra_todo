ece79d117534c6b89a57847de007e9d99e296b9b
Kevin Read <readkev@gmail.com>
2010-04-29 19:28:37 +1000

    tabs to spaces

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,58 +1,58 @@
 class TodoItemsController < ApplicationController
-	def index
-		if params[:tags] && @tag = Tag.find_by_name( params[ :tags ] )
-			@todoItems = @tag.todo_items
-		else
-			@todoItems = TodoItem.all( :order => "created_at desc" )
-		end
-		@TodoItem = TodoItem.new
-	end
-	
-	def create
-		@todoItem = TodoItem.new( params[ :todo_item ] )
-		if params[ :tag ]			
-			@tag = Tag.find( params[ :tag ] )
-			@todoItem.tags << @tag
-		end
-			
-		if @todoItem.save!
-			redirect_to root_path
-		else
-			@todoItems = TodoItem.all( :order => "created_at desc" )
-			render :action => "index"
-		end
-	end
-	
-	def destroy
-		TodoItem.destroy params[ :id ] 
-		redirect_to root_path
-	end
+  def index
+    if params[:tags] && @tag = Tag.find_by_name( params[ :tags ] )
+      @todoItems = @tag.todo_items
+    else
+      @todoItems = TodoItem.all( :order => "created_at desc" )
+    end
+    @TodoItem = TodoItem.new
+  end
+  
+  def create
+    @todoItem = TodoItem.new( params[ :todo_item ] )
+    if params[ :tag ]      
+      @tag = Tag.find( params[ :tag ] )
+      @todoItem.tags << @tag
+    end
+      
+    if @todoItem.save!
+      redirect_to root_path
+    else
+      @todoItems = TodoItem.all( :order => "created_at desc" )
+      render :action => "index"
+    end
+  end
+  
+  def destroy
+    TodoItem.destroy params[ :id ] 
+    redirect_to root_path
+  end
 
-	def show
-		@todo_item = TodoItem.find( params[ :id ] )
-		@todo_item_tag = TodoItemTag.new( :tag_id => @todo_item.id)
-	end
-	
-	def done
-		update_item{ @todo_item.done = true }
-	end
-	
-	def undo
-		update_item{ @todo_item.done = false }
-	end
+  def show
+    @todo_item = TodoItem.find( params[ :id ] )
+    @todo_item_tag = TodoItemTag.new( :tag_id => @todo_item.id)
+  end
+  
+  def done
+    update_item{ @todo_item.done = true }
+  end
+  
+  def undo
+    update_item{ @todo_item.done = false }
+  end
 
-	def update_tags		
-		update_item do
-			tags = params[ :tags ].split(',').map{ |s| s.strip }.map{ |s| Tag.find_or_create_by_name( s ) }
-			@todo_item.tags = tags
-		end
-	end
+  def update_tags    
+    update_item do
+      tags = params[ :tags ].split(',').map{ |s| s.strip }.map{ |s| Tag.find_or_create_by_name( s ) }
+      @todo_item.tags = tags
+    end
+  end
 
-	private
-		def update_item
-			@todo_item = TodoItem.find( params[ :id ] )
-			yield
-			@todo_item.save!
-			redirect_to  request.env['HTTP_REFERER']
-		end
+  private
+    def update_item
+      @todo_item = TodoItem.find( params[ :id ] )
+      yield
+      @todo_item.save!
+      redirect_to  request.env['HTTP_REFERER']
+    end
 end
--- db/migrate/20100422094101_create_todo_item_tags.rb
+++ db/migrate/20100422094101_create_todo_item_tags.rb
@@ -8,13 +8,13 @@ class CreateTodoItemTags < ActiveRecord::Migration
     end
     
     add_index :todo_item_tags, :tag_id
-	add_index :todo_item_tags, :todo_item_id
-	add_index :todo_item_tags, [:tag_id, :todo_item_id]
+  add_index :todo_item_tags, :todo_item_id
+  add_index :todo_item_tags, [:tag_id, :todo_item_id]
   end
 
   def self.down
-  	remove_index :todo_item_id, :column_name
-  	remove_index :todo_item_tags, :column_name
+    remove_index :todo_item_id, :column_name
+    remove_index :todo_item_tags, :column_name
     drop_table :todo_item_tags    
   end
 end
